21b318fd3b2708b7df27574e1782694aef971512
DDOShub.com <133521390+ddoshub@users.noreply.github.com>
2026-04-22 15:30:34 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'static',   // 关键！必须 static
+  output: 'static',
   adapter: cloudflare(),
 });
